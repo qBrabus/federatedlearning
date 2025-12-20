5c9680b096e4ca51e42a160b8eea3c78eb21b945
qBrabus <94984398+qBrabus@users.noreply.github.com>
2025-12-20 01:00:20 +0100

    Update run.sh

--- orchestrator/run.sh
+++ orchestrator/run.sh
@@ -2,12 +2,10 @@
 set -euo pipefail
 
 APPIO_API_ADDRESS=${APPIO_API_ADDRESS:-"superlink:9091"}
-NUM_ROUNDS=${NUM_ROUNDS:-5}
 
 echo "[serverapp] Connexion à ${APPIO_API_ADDRESS}"
 
 flower-superexec \
   --plugin-type serverapp \
   --appio-api-address "${APPIO_API_ADDRESS}" \
-  --run-config "num-server-rounds=${NUM_ROUNDS}" \
   --insecure
